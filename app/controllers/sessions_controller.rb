a4f8dc2ab9c3e17fe713698675e0ad77fd02966a
Ramin Mammadzada <remki-design@Hotmail.com>
2020-11-02 23:00:16 +0300

    Add simple sessions controller and relevant view files.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by_(params[:username])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'You are successfully logged in!'
+    else
+      flash.now[:alert] = 'You username is not valid!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'You are logged out!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   resources :followings
   resources :opinions
   resources :users
+
+  root "opinions#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
