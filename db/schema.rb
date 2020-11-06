febcdb8e687e32c014071853304fbe8dd79c7553
Ramin Mammadzada <remki-design@Hotmail.com>
2020-11-06 13:48:41 +0300

    Update databasee configuration file to switch from  SQLite to PostgreSQL.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_05_175512) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "followings", force: :cascade do |t|
     t.integer "followerId"
     t.integer "followedId"
