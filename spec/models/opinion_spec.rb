8b82b1046aa80e7e85217d311ba098ceab6676a9
Ramin Mammadzada <remki-design@Hotmail.com>
2020-11-07 15:54:24 +0300

    Update rspec tests for Opinion model.

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -1,20 +1,20 @@
-# require 'rails_helper'
-#
-# RSpec.describe Opinion, type: :model do
-#   let(:test_user) { User.create(username: 'ramin', full_name: 'Ramin Mammadzada') }
-#   let(:test_opinion) { described_class.new(text: 'This is the text.', author_id: test_user.id) }
-#
-#   describe 'validations' do
-#     it 'should be valid for everything is valid' do
-#       expect(test_opinion).to be_valid
-#     end
-#     it "should be invalid for the author_id doesn't exist" do
-#       test_opinion.author_id = 3
-#       expect(test_opinion).to be_invalid
-#     end
-#   end
-#
-#   describe 'associations' do
-#     it { is_expected.to belong_to(:user) }
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Opinion, type: :model do
+  let(:test_user) { User.create(username: 'ramin', fullname: 'Ramin Mammadzada') }
+  let(:test_opinion) { described_class.new(text: 'This is the text.', authorId: test_user.id) }
+
+  describe 'validations' do
+    it 'should be valid so, the opinion is created is valid' do
+      expect(test_opinion).to be_valid
+    end
+    it "should be invalid for the author_id doesn't exist" do
+      test_opinion.authorId = 3
+      expect(test_opinion).to be_invalid
+    end
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+end
